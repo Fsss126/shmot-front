1a98a7ec0e01ef832fbf97e48a6061f29c8cab97
Fsss126 <32033964+Fsss126@users.noreply.github.com>
2018-02-26 05:07:51 +0300

    Subscriptions added

--- js/search.js
+++ js/search.js
@@ -1,3 +1,5 @@
+var loading = false;
+
 $(document).ready(function() {
     $('.content .search_btn').on('click',function (e) {
         if ($(this).prev().val() != "")
@@ -10,4 +12,18 @@ $(document).ready(function() {
     });
     //adds search key to the search field
     $('#search_key').next().children('input').val($('#search_key').text());
+
+    $(window).scroll(function () {
+        //load more ads when user reaches the bottom
+        //TODO: add ajax query
+        if($(window).scrollTop() + $(window).height() > $(document).height() - 400 && !loading) {
+            loading = true;
+            $('.spinner').removeClass('hidden');
+            setTimeout(function () {
+                $('.spinner').addClass('hidden');
+                $('#ads').append($('#ads').html());
+                loading = false;
+            }, 2000);
+        }
+    });
 });
